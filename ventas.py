f442ae7930c56b9622003fa5dcfe87ccabc05cfe
monse1224 <A01709064@tec.mx>
2024-04-15 18:34:52 -0600

    ventas

--- ventas.py
+++ ventas.py
@@ -2,4 +2,17 @@ import pandas as pd
 import numpy as np
 
 df=pd.read_csv("ventas_totales.csv")
-print(df.head())
+#print(df.head()) 
+#print(df.info())
+#print(df.describe())
+#print(df.isnull().sum())
+
+#quitar un nulo en l columna de salon_ventas
+df['salon_ventas'] = df['salon_ventas'].fillna(round(df['salon_ventas'].mean(),1))
+#valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#quitar nulos de tarjeta de débito con media
+df['tarjetas_debito'] = df['tarjetas_debito'].fillna(round(df['tarjetas_debito'].median(),1))
+valores_nulos=df.isnull().sum()
+print(valores_nulos)
