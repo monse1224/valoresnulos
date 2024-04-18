38d926f3ec9200886a66e4a8e74c7cd5b109be3e
monse1224 <A01709064@tec.mx>
2024-04-18 13:08:38 -0600

    clientes

---
+++ clientes.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df=pd.read_excel("clientes.xlsx")
+#print(df.isnull().sum())
+
+#rellenamos con la composición de caracteres que tiene una curp (18 digitos)
+df['RFC'].fillna('XXXX-000000-X-XX-XXX-00', inplace=True) 
+#print(df.isnull().sum())
+
+#indicamos que no hay nombre en el campo, para no eliminar al cliente
+df['NOMBRE'].fillna('Sin Nombre', inplace=True)
+print(df.isnull().sum())
