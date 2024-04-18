b51d93cc2b69ed4eed8bd92cd68e26a13bfee211
monse1224 <A01709064@tec.mx>
2024-04-18 13:17:04 -0600

    devoluciones

---
+++ devoluciones.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df=pd.read_excel("devoluciones.xlsx")
+#print(df.isnull().sum())
+
+#ingresamos el formato de una fecha inexistente para indicar fecha invalida.
+df['FECHA_CANCELA'].fillna('00/00/0000  00:00:00 x. x.', inplace=True) 
+#print(df.isnull().sum())
+
+#rellenamos con el formato o composición de datos que contiene la columna, para identificarlo como inexistente
+df['DOC_ANT'].fillna('X00000', inplace=True) 
+print(df.isnull().sum())
